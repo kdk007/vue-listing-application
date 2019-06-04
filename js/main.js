5479f8cb4093b2537542bc10d95b795382281b91
kdk007 <1585432244@qq.com>
2019-06-04 22:18:47 +0800

    初始化提交

---
+++ js/main.js
@@ -0,0 +1,146 @@
+;(function () {
+    'use strict';
+
+    var Event = new Vue();
+
+    var alert_sound = document.getElementById('alert-sound');
+
+    function copy(obj) {
+        return Object.assign({},obj);
+    }
+
+    Vue.component('task',{
+        template: '#task-tpl',
+        props: ['todo'],
+        methods: {
+            action: function (name,params) {
+                Event.$emit(name,params);
+            }
+        }
+    })
+    new  Vue({
+        el: '#main',
+        data: {
+            list: [],
+            last_id: 0,
+            current: {}
+        },
+        mounted: function(){
+            var me = this;
+            this.list  = ms.get('list') || this.list;
+            this.last_id  = ms.get('last_id') || this.last_id;
+
+            setInterval(function () {
+                me.check_alerts();
+            },1000);
+            Event.$on('remove',function (id) {
+                if(id){
+                    me.remove(id);
+                }
+            });
+
+            Event.$on('toggle_complete',function (id) {
+                if(id){
+                    me.toggle_complete(id);
+                }
+            });
+
+            Event.$on('set_current',function (id) {
+                if(id){
+                    me.set_current(id);
+                }
+            });
+
+            Event.$on('toggle_detail',function (id) {
+                if(id){
+                    me.toggle_detail(id);
+                }
+            });
+        },
+        methods: {
+            check_alerts: function(){
+              var me =  this;
+              this.list.forEach(function (row,i) {
+                console.log('row:',row);
+
+                var alert_at = row.alert_at;
+                if(!row.alert_at || row.alert_confirmed) return;
+
+                var alert_at = (new Date(alert_at)).getTime();
+                var now = (new Date()).getTime();
+
+                if(now >= alert_at) {
+                    alert_sound.play();
+                    var confirmed = confirm(row.title);
+                    Vue.set(me.list[i],'alert_confirmed',confirmed);
+                }
+
+              })
+            },
+            //添加和更新
+            merge: function () {
+                var is_update,id;
+                is_update = id = this.current.id;
+                if(is_update){
+                    var index = this.find_index(id);
+
+                    Vue.set(this.list,index,copy(this.current));
+                }else{
+                    var title=this.current.title;
+                    if(!title && title!==0) return;
+
+                    var todo= copy(this.current);
+                //实现自增id机制
+                    this.last_id++;
+                    ms.set('last_id',this.last_id);
+                    todo.id = this.last_id;
+
+                    this.list.push(todo);
+                }
+                this.reset_current();
+
+            },
+            //查看详情
+            toggle_detail: function(id){
+                var index = this.find_index(id);
+                Vue.set(this.list[index],'show_detail',!this.list[index].show_detail);
+            },
+            //删除
+            remove: function (id) {
+                var index = this.find_index(id);
+                this.list.splice(index,1);
+            },
+            // next_id: function () {
+            //     return this.list.length + 1;
+            // },
+            set_current: function (todo) {
+                this.current = copy(todo);
+            },
+            reset_current: function () {
+                this.set_current({});
+            },
+            find_index: function(id){
+                return this.list.findIndex(function (item) {
+                    return item.id == id;
+                });
+            },
+            //控制完成状态
+            toggle_complete: function (id) {
+                var i=this.find_index(id);
+                Vue.set(this.list[i],'completed',!this.list[i].completed);
+            }
+        },
+        watch: {
+            list: {
+                deep: true,
+                handler: function(n,o){
+                    if(n){
+                        ms.set('list',n);
+                    }else{
+                        ms.set('list',[]);
+                    }
+                }
+            }
+        }
+    })
+})();
---
+++ js/myStorage.js
@@ -0,0 +1,21 @@
+;(function () {
+    window.ms={
+        set: set,
+        get: get,
+    };
+    function set(key,val) {
+
+        localStorage.setItem(key,JSON.stringify(val));
+    }
+    function get(key) {
+        var json=localStorage.getItem(key);
+        if(json){
+            return JSON.parse(json);
+        }
+    }
+
+})();
+
+    ms.set('name','王花花')
+    var name = ms.get('name');
+    console.log('name:', name);
